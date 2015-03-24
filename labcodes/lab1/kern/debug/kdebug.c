3392668bfaa5a41ebca9173b62c6e1c10fe2b89e
largelyfs <largelymfs@gmail.com>
2015-03-24 20:01:02 +0800

    Question 5

--- labcodes/lab1/kern/debug/kdebug.c
+++ labcodes/lab1/kern/debug/kdebug.c
@@ -302,5 +302,21 @@ print_stackframe(void) {
       *           NOTICE: the calling funciton's return addr eip  = ss:[ebp+4]
       *                   the calling funciton's ebp = ss:[ebp]
       */
+      uint32_t ebp = read_ebp();
+      uint32_t eip = read_eip();
+      int i = 0;
+      int p = 0;
+      while (ebp != 0){
+        if (i == STACKFRAME_DEPTH) break;
+        cprintf("ebp:0x%08x eip:0x%08x args:", ebp, eip);
+        uint32_t * args = (uint32_t *)(ebp) + 2;
+        for (p = 0; p < 4; p++)
+            cprintf("0x%08x", args[p]);
+        cprintf("\n");
+        print_debuginfo(eip - 1);
+        eip = ((uint32_t * ) ebp)[1];
+        ebp = ((uint32_t * ) ebp)[0];
+        i++;
+      }
 }
 
